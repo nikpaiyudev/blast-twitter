f303beefec64da1530e5fd0a712b10ff30489062
nikpaiyudev <nikhilradhakrishnan.work@gmail.com>
2023-12-21 10:29:19 +0530

    Twitter Integration changes

--- src/app/components/Twitter.tsx
+++ src/app/components/Twitter.tsx
@@ -11,14 +11,21 @@ const Twitter = () => {
 
     const { initateTwitterAuth, getTwitterUserInfo } = useTwitterFlow();
     const [twitterUser, setTwitterUser] = useState<ITwitterUser>();
+    const [isLoading, setLoading] = useState(false);
     const params = useSearchParams();
     const state = params.get('state');
     const code = params.get('code');
-
     const getTwitterUser = async () => {
         if (params.get('state') && params.get('code')) {
-            const user = await getTwitterUserInfo(state as string, code as string, 'http://localhost:3000');
-            setTwitterUser(user);
+            try {
+                setLoading(true);
+                const user = await getTwitterUserInfo(state as string, code as string, 'http://localhost:3000');
+                setTwitterUser(user);
+                setLoading(false);
+            } catch (e) {
+                console.error(e, 'Getting Twitter User Error');
+                setLoading(false);
+            }
         }
     }
 
@@ -33,18 +40,21 @@ const Twitter = () => {
 
     return (
         <div className="flex flex-col justify-center items-center gap-10 w-full pt-40 pb-40">
-            <RenderIf isTrue={false}>
+            <RenderIf isTrue={!isLoading && !Boolean(twitterUser)}>
                 <>
-                    <div className="flex flex-col gap-5">
+                    <div className="flex flex-col gap-3">
                         <span className="text-3xl font-thin italic text-center">Reserve Your Blast <span className="text-[#FFFF33]">Name.</span></span>
                         <p className="italic font-normal text-lg text-center">The only Ethereum L2 with native yield for ETH and stablecoins.</p>
                     </div>
                     <Button name="Connect Twitter" onClick={handleTwitter}></Button>
                 </>
             </RenderIf>
-            <RenderIf isTrue={true}>
+            <RenderIf isTrue={isLoading && !Boolean(twitterUser)}>
+                <span className="text-2xl text-center italic">Getting yout twitter handle...</span>
+            </RenderIf>
+            <RenderIf isTrue={!isLoading && Boolean(twitterUser)}>
                 <div className="w-full flex justify-center items-center">
-                    <span className="text-3xl text-center">Your X/Twitter handle,{twitterUser?.username}, has been reserved for an <br/><span className="text-[#FFFF33]">onBlast.eth </span>subdomain!</span>
+                    <span className="text-3xl text-center italic">Your X/Twitter handle,<span className="text-[#FFFF33]"> {twitterUser?.username}</span>, has been reserved for an <br /><span className="text-[#FFFF33]"> onBlast.eth </span>subdomain!</span>
                 </div>
             </RenderIf>
         </div>
